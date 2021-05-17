35ce812d9241f6720fa679c65bfba19ec0873608
congnb-dev <70565602+congnb-dev@users.noreply.github.com>
2021-05-17 14:49:06 +0700

    Add files via upload

---
+++ soNguyenTo.js
@@ -0,0 +1,38 @@
+
+function kiem_tra_snt()
+  {
+      // Biến cờ hiệu
+      var flag = true;
+
+      var n = document.getElementById("number").value;
+
+      // Nếu n bé hơn 2 tức là không phải số nguyên tố
+      if (n < 2){
+          flag = false;
+      }
+	  else if (n == 2){
+          flag = true;
+      }
+      else if (n % 2 == 0){
+		flag = false;
+      }
+      else{
+          // lặp từ 3 tới n-1 với bước nhảy là 2 (i+=2)
+          for (var i = 3; i < n-1; i+=2)
+          {
+              if (n % i == 0){
+                  flag = false;
+                  break;
+              }
+          }
+      }
+
+      // Kiểm tra biến flag
+      if (flag == true){
+          document.write(n + " là số nguyên tố <br/>");
+      }
+      else{
+          document.write(n + " không phải là số nguyên tố <br/>");
+      }
+  }
+
